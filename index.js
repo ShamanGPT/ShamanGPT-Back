4c5ed20f28ddb283f86ce96abbd5812573c8fe92
Sebastian Caicedo <sdrivert@hotmail.com>
2024-03-05 14:44:31 -0500

    Add holistic doctor prompt

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.listen(port, () => {
 // Initialize OpenAI API
 
 const openai = new OpenAI({
-  apiKey: process.env.OPENAI_API_KEY // This is also the default, can be omitted
+  apiKey: "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" // This is also the default, can be omitted
 });
 
 // Define a route to handle questions
@@ -31,9 +31,15 @@ app.post('/ask', async (req, res) => {
   const userMessage = req.body.message;
 
   // Call the OpenAI API to generate an answer
+  var prompt = `Quiero que actúes como médico holístico e idees tratamientos creativos para enfermedades o dolencias. 
+  Deberá ser capaz de recomendar medicamentos convencionales, hierbas medicinales y otras alternativas naturales. 
+  También deberá tener en cuenta la edad, el estilo de vida y el historial médico del paciente a la hora de ofrecer sus recomendaciones. 
+  Recuerde responder en no más de 250 caracteres.
+  Mi primera solicitud es elaborar un plan de tratamiento centrado en métodos curativos holísticos para un paciente que tiene los siguientes síntomas: ".
+  `
   const chatCompletion = await openai.chat.completions.create({
     model: "gpt-3.5-turbo",
-    messages: [{"role": "user", "content": userMessage}],
+    messages: [{"role": "user", "content": prompt + userMessage}],
   });
   res.send(chatCompletion.choices[0].message);
 });
